5a3827615b004d5b355e84928f048d984d075e8d
Tim Pambor <t.pambor@uniandes.edu.co>
2024-04-03 00:07:02 +0200

    Preparar para RDS

--- app.py
+++ app.py
@@ -1,22 +1,31 @@
+import os
 from flask import Flask
 from db import db
-from environment_variables import (
-    DB_USER,
-    DB_PASSWORD,
-    DB_HOST,
-    DB_PORT,
-    DB_NAME,
-)
 
+def get_rds_url():
+    if not all(
+        key in os.environ
+        for key in [
+            "RDS_USERNAME",
+            "RDS_PASSWORD",
+            "RDS_HOST",
+            "RDS_PORT",
+            "RDS_DB_NAME",
+        ]
+    ):
+        return None
+
+    return f"postgresql+pg8000://{os.environ['RDS_USERNAME']}:{os.environ['RDS_PASSWORD']}@{os.environ['RDS_HOST']}:{os.environ['RDS_PORT']}/{os.environ['RDS_DB_NAME']}"
 
 # Create the Flask application
 def create_app(database=None):
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = (
-            database
-            or f"postgresql+pg8000://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-    )
+    app.config["SQLALCHEMY_DATABASE_URI"] = database or get_rds_url()
+
+    if app.config["SQLALCHEMY_DATABASE_URI"] is None:
+        print("Please set the RDS environment variables")
+        exit(1)
 
     db.init_app(app)
     with app.app_context():
--- environment_variables.py
+++
@@ -1,8 +0,0 @@
-import os
-
-# Database
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
